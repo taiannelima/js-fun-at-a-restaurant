98127571705fc4d73e81055372ab05261f4f3e20
Taianne Lima <rtaianne@gmail.com>
2023-12-02 13:59:52 -0800

    order.js done

--- src/order.js
+++ src/order.js
@@ -1,7 +1,50 @@
 
-module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+function takeOrder(order, deliveryOrders) {
+  if(deliveryOrders.length < 3) {
+    deliveryOrders.push(order);
+  }
 }
+
+function refundOrder(orderNumber, deliveryOrders) {
+
+  for (var i = deliveryOrders.length - 1; i >= 0; i--) {
+    if (deliveryOrders[i].orderNumber === orderNumber) {
+      deliveryOrders.splice(i, 1);
+    }
+  }
+  
+}
+
+function listItems(deliveryOrders) {
+  var items = "";
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if(items != "") {
+      items = items + ", " +  deliveryOrders[i].item;  
+    } else {
+      items = deliveryOrders[i].item;  
+    }
+  }
+  return items;
+}
+
+function searchOrder(deliveryOrders, itemName) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if(deliveryOrders[i].item === itemName) {
+      return true;
+    }
+  }
+  return false;
+}
+
+module.exports = {
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
+}
+
+
+
+
+
+
